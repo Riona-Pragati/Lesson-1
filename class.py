3a5629a8a5f1f0296de7d9697680323ae1226064
Riona-Pragati <renuprasanna2012@gmail.com>
2026-02-03 19:26:12 -0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,17 @@
+class Expression:
+    def __init__(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+    def add_numbers(self):
+        result = self.num1 + self.num2 + self.num3
+        print("The sum is:", result)
+
+
+# Creating objects and calling the method
+exp1 = Expression(10, 20, 30)
+exp1.add_numbers()
+
+exp2 = Expression(5, 7, 9)
+exp2.add_numbers()
